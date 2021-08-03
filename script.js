1d46313b06bd827838717cf7a268b709226ceee8
Justin Damon <justindamon84@gmail.com>
2021-08-02 21:01:54 -0400

    heart icon

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function setColor(btn, color){
-    var count=1;
-    var property = document.getElementById(btn);
-    if (count == 0){
-        property.style.color = "black"
-        count=1;
-    }
-    else{
-        property.style.color = "red"
-        count=0;
-    }
-}  
+$(".heart.fa").click(function() {
+    $(this).toggleClass("fa-heart fa-heart-o");
+  });
